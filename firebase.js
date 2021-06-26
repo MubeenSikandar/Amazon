edf735dd8c6e0ef60570d226e0950dd56ff1d514
MubeenSikandar <68024800+MubeenSikandar@users.noreply.github.com>
2021-06-26 11:47:12 +0500

    better

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBdFY0NBvsc9Y1fqNetDpKRToCyG8-ikRs",
+    authDomain: "clone-50ed5.firebaseapp.com",
+    projectId: "clone-50ed5",
+    storageBucket: "clone-50ed5.appspot.com",
+    messagingSenderId: "473637973845",
+    appId: "1:473637973845:web:2646a72f8e30d33bf009dc",
+    measurementId: "G-S89Q2H7T1K"
+  };
